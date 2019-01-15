c6bf8f72d715a379bbb621733f5c9ad27f779eee
Rob Blanckaert <basicer@basicer.com>
2019-01-14 17:45:19 -0800

    Prettier

--- gen/index.js
+++ gen/index.js
@@ -1,46 +1,47 @@
-const fs = require('fs');
-const path = require('path');
-const glob = require('glob');
-const toml = require('toml');
-const ejs = require('ejs');
+const fs = require("fs");
+const path = require("path");
+const glob = require("glob");
+const toml = require("toml");
+const ejs = require("ejs");
 
-const base = path.join(__dirname, '..');
-const dest = path.join(base, 'out');
+const base = path.join(__dirname, "..");
+const dest = path.join(base, "out");
 
+const btoa = b => Buffer.from(b).toString("base64");
 
-const btoa = (b) => Buffer.from(b).toString('base64');
+let packages = fs
+	.readFileSync(path.join(base, "packages.txt"), "utf8")
+	.split(/\r?\n/);
 
-let packages = fs.readFileSync(path.join(base, 'packages.txt'), 'utf8').split(/\r?\n/);
-
-let list = glob.sync(path.join(base, 'languages', '*.toml'));
+let list = glob.sync(path.join(base, "languages", "*.toml"));
 let languages = [];
 list.sort();
 
 let dc = [];
 function undup(line) {
-	if ( dc.indexOf(line) == -1 ) {
+	if (dc.indexOf(line) == -1) {
 		dc.push(line);
 		return line;
 	} else {
-		return '#' + line;
+		return "#" + line;
 	}
 }
 
-for ( let file of list ) {
-	let info = toml.parse(fs.readFileSync(file, 'utf8'));
+for (let file of list) {
+	let info = toml.parse(fs.readFileSync(file, "utf8"));
 
 	let name = info.name;
 
-	if ( process.env.LANGS ) {
-		let set =  process.env.LANGS.split(/[ ,]+/);
-		if ( set.indexOf(name) == -1 ) continue;
+	if (process.env.LANGS) {
+		let set = process.env.LANGS.split(/[ ,]+/);
+		if (set.indexOf(name) == -1) continue;
 	}
 
 	info.popularity = info.popularity || 2;
 
 	if (!info.versionCommand) {
 		let flag = "--version";
-		if ( ["lua"].indexOf(info.name) !== -1 ) {
+		if (["lua"].indexOf(info.name) !== -1) {
 			flag = "-v";
 		}
 
@@ -52,36 +53,33 @@ for ( let file of list ) {
 			flag = "-version";
 		}
 
-		if ( info.compile ) {
-			info.versionCommand = [info.compile.command[0], flag]
-		} else if ( info.run ) {
-			info.versionCommand = [info.run.command[0], flag]
+		if (info.compile) {
+			info.versionCommand = [info.compile.command[0], flag];
+		} else if (info.run) {
+			info.versionCommand = [info.run.command[0], flag];
 		} else {
-			info.versionCommand = ['/bin/false']
+			info.versionCommand = ["/bin/false"];
 		}
 	}
 
-	if ( 'tests' in info ) {
-
+	if ("tests" in info) {
 	} else {
 		console.log(`Warning ${name} has no tests.`);
 		info.tests = {};
 	}
 
-	if ( info.packages ) {
-		for ( let p of info.packages ) {
-			if ( packages.indexOf(p) == -1 ) packages.push(p);
+	if (info.packages) {
+		for (let p of info.packages) {
+			if (packages.indexOf(p) == -1) packages.push(p);
 		}
 	}
 	languages.push(info);
 }
 
-
-
 let lbypop = JSON.parse(JSON.stringify(languages));
-lbypop.sort((a,b) => b.popularity - a.popularity);
+lbypop.sort((a, b) => b.popularity - a.popularity);
 
-let lpad = (s,n) => s + new Array(n - s.length).fill(' ').join('');
+let lpad = (s, n) => s + new Array(n - s.length).fill(" ").join("");
 
 let ctx = {
 	languages,
@@ -90,26 +88,27 @@ let ctx = {
 	packages,
 	undup,
 	lpad,
-	c: (a) => a.join(' ')
+	c: a => a.join(" ")
 };
 
 let objects = {
-	'test.sh': 'tests.ejs',
-	'self-test': 'inside-test.ejs',
-	'phase1.sh': 'phase1.ejs',
-	'phase2.sh': 'phase2.ejs',
-	'run-project': 'run-project.ejs',
-	'detect-language': 'detect-language.ejs',
-	'polygott-survey': 'versions.ejs'
-}
+	"test.sh": "tests.ejs",
+	"self-test": "inside-test.ejs",
+	"phase1.sh": "phase1.ejs",
+	"phase2.sh": "phase2.ejs",
+	"run-project": "run-project.ejs",
+	"detect-language": "detect-language.ejs",
+	"polygott-survey": "versions.ejs"
+};
 
-for ( let target in objects ) {
+for (let target in objects) {
 	let tp = path.join(dest, target);
 	fs.writeFileSync(
 		tp,
-		ejs.compile(fs.readFileSync(path.join(__dirname, objects[target]), 'utf8'))(ctx),
-		'utf8'
+		ejs.compile(fs.readFileSync(path.join(__dirname, objects[target]), "utf8"))(
+			ctx
+		),
+		"utf8"
 	);
 	fs.chmodSync(path.join(dest, target), 0o755);
 }
-
