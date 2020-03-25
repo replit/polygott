4914c2a8e3f93def1f6b4b9319f91bf55246956f
Dan Stowell <danstowell@fastmail.com>
2020-03-25 09:42:49 -0400

    install prybar_assets at /usr/bin/prybar_assets

--- fetch-prybar.sh
+++ fetch-prybar.sh
@@ -5,3 +5,4 @@ mkdir -p /gocode/src/github.com/replit/
 wget "https://github.com/replit/prybar/archive/${TAG}.zip"
 unzip "${TAG}.zip"
 mv "prybar-${TAG}" /gocode/src/github.com/replit/prybar/
+cp -r /gocode/src/github.com/replit/prybar/prybar_assets /usr/bin/prybar_assets/
