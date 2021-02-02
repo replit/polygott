046b009e88b518d4c3b3f509bd0fba19ca70d2af
lhchavez <lhchavez@lhchavez.com>
2021-02-02 10:16:21 -0800

    Use `polygott-phase0` as base image for the prybar resources (#212)
    There is no need to involve alpine, since we already have a perfectly
    reasonable base image already. This also gets rid of the fetch-prybar.sh
    script for simplicity.

--- extra/fetch-prybar.sh
+++
@@ -1,8 +0,0 @@
-TAG="$1"
-
-mkdir -p /gocode/src/github.com/replit/
-
-wget "https://github.com/replit/prybar/archive/${TAG}.zip"
-unzip "${TAG}.zip"
-mv "prybar-${TAG}" /gocode/src/github.com/replit/prybar/
-cp -r /gocode/src/github.com/replit/prybar/prybar_assets /usr/bin/prybar_assets/
